c5236c00b9183491d96c5c0800d609df3922d14d
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2024-03-04 03:33:12 -0500

    add cart functionality

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,14 +1,16 @@
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
   const {data: products, isLoading, error} = useGetProductsQuery();
 
   return (
     <>
-      { isLoading? (<h2>Loading...</h2>) 
-        : (error? (<div>{error?.data?.message || error.error}</div>) 
+      { isLoading? (<Loader />) 
+        : (error? (<Message variant='danger'>{error?.data?.message || error.error}</Message>) 
           : ( <>
                 <h1> Latest Products </h1>
                 <Row>
@@ -23,8 +25,6 @@ const HomeScreen = () => {
             )
           )
         }
-        
-
     </>
   )
 }
