05767111d76a9dadd83f6bd049f31fad9513440e
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2024-06-26 02:48:05 -0400

    logout func complete

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,31 @@
+import { useNavigate } from 'react-router-dom';
 import {Badge, Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
 import {FaShoppingCart,FaUser} from 'react-icons/fa';
 import {LinkContainer} from 'react-router-bootstrap';
-import { useSelector } from 'react-redux';
-import Adab from '../assets/Adab.png'
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
+import Adab from '../assets/Adab.png';
 //<img src={Adab} alt='Adab' width="200" height="100"/>
 
 const Header = () => {
     const { cartItems } = useSelector((state) => state.cart);
     const { userInfo } = useSelector((state) => state.auth);
 
-    const logoutHandler = () => {
-        console.log("logout");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+
+    const logoutHandler = async() => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate('/login');
+        } catch (error) {
+            console.log(error);
+        }
     };
 
   return (
