de9dc3e99d72266fedaf96b9a09c08b70fc4284c
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2024-06-27 01:08:43 -0400

    Payment screen setup

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart") 
     ? JSON.parse(localStorage.getItem("cart")) 
-    : {cartItems: [], shippingAddress: {}, paymentMethod: 'Paypal' };
+    : {cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 const cartSlice = createSlice({
     name: "cart",
@@ -31,10 +31,14 @@ const cartSlice = createSlice({
         saveShippingAddress: (state, action) => {
             state.shippingAddress = action.payload;
             return updateCart(state);
-        }
+        },
+        savePaymentMethod: (state, action) => {
+            state.paymentMethod = action.payload;
+            return updateCart(state);
+        },
     },
 });
 
-export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod } = cartSlice.actions;
 
 export default cartSlice.reducer;
