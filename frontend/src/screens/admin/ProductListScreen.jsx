3efc047df51ec3a18fdbe7fb4fd8ac06f25c4af4
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2024-07-13 14:52:04 -0400

    create new product

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -3,13 +3,27 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa';
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
-import { useGetProductsQuery } from '../../slices/productsApiSlice';
+import { toast } from 'react-toastify';
+import { useGetProductsQuery, useCreateProductMutation } from '../../slices/productsApiSlice';
 
 const ProductListScreen = () => {
-    const { data: products, isLoading, error} = useGetProductsQuery();
+    const { data: products, isLoading, error, refetch} = useGetProductsQuery();
+    
+    const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
 
     const deleteHandler = (productId) => {
         console.log('delete', productId);
+    };
+
+    const createProductHandler = async () => {
+        if (window.confirm('Create new product?')) {
+            try {
+                await createProduct();
+                refetch();
+            } catch (error) {
+                toast.error(error?.data?.message || error.error);
+            }
+        }
     };
     
   return (
@@ -19,12 +33,12 @@ const ProductListScreen = () => {
                 <h1>Products</h1>
             </Col>
             <Col className='text-end'>
-                <Button className='btn-sm m-3'>
+                <Button className='btn-sm m-3' variant='success' style={{ color: 'white'}} onClick={createProductHandler}>
                     <FaEdit /> Create Product
                 </Button>
             </Col>
         </Row>
-
+        {loadingCreate && <Loader/>}
         {isLoading ? (<Loader/>) : (
             error ? (<Message variant='danger'>{error}</Message>) : (
                 <>
