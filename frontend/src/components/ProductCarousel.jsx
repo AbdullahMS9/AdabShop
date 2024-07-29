b9298692fb678544ca60363b2057d46cb15a2508
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2024-07-29 10:24:39 -0400

    Carousel Centered, Product Images Added

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -11,9 +11,9 @@ const ProductCarousel = () => {
     : (
     <Carousel pause='hover' className='bg-primary mb-4'>
         {products.map((product) => (
-            <Carousel.Item key={product._id}>
-                <Link to={`/product/${product._id}`}>
-                    <Image src={product.image} alt={product.name} fluid />
+            <Carousel.Item key={product._id} >
+                <Link to={`/product/${product._id}`} class="d-flex justify-content-center">
+                    <Image src={product.image} alt={product.name} fluid width='50%' height='auto' />
                     <Carousel.Caption className='carousel-caption'>
                         <h2>
                             {product.name} (${product.price})
